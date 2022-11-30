ce7b7f03aaebafca0d7dc5bc4974e1c9df339010
e-stan <estancl1234@gmail.com>
2022-11-30 11:55:38 -0600

    remove cython

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 from setuptools import find_packages
-from Cython.Build import cythonize
-import numpy as np
+#from Cython.Build import cythonize
+#import numpy as np
 
 # from distutils.core import setup
 
 setup(
     name="SpectralEntropy",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(exclude=['*tests*']),
     python_requires='>=3.7',
     url="https://github.com/e-stan/SpectralEntropy",
-    download_url='https://github.com/e-stan/SpectralEntropy/archive/v1.0.0.tar.gz',
+    download_url='https://github.com/e-stan/SpectralEntropy/archive/v1.0.1.tar.gz',
     license="Apache Software License 2.0",
     install_requires=[
         "numpy>=1.17.4",
@@ -20,16 +20,16 @@ setup(
         "pytest",
         "pytest-cov"
     ],
-    ext_modules=cythonize("spectral_entropy/tools_fast.pyx",
-                          annotate=True,
-                          compiler_directives={
-                              'language_level': "3",
-                              'cdivision': True,
-                              'boundscheck': False,  # turn off bounds-checking for entire function
-                              'wraparound': False  # turn off negative index wrapping for entire function
-                          }),
+    # ext_modules=cythonize("spectral_entropy/tools_fast.pyx",
+    #                       annotate=True,
+    #                       compiler_directives={
+    #                           'language_level': "3",
+    #                           'cdivision': True,
+    #                           'boundscheck': False,  # turn off bounds-checking for entire function
+    #                           'wraparound': False  # turn off negative index wrapping for entire function
+    #                       }),
     test_suite="tests",
-    include_dirs=[np.get_include()]
+    #include_dirs=[np.get_include()]
 )
 
 # python setup.py build_ext --inplace
